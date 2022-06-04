3292ada7506d407028a84e164cbbd1244e4d3784
emrsyhn <emirseyhana.z@gmail.com>
2022-06-04 12:41:07 +0300

    anasayfa eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proje/Anasayfa.dart';
 import 'package:proje/bulut.dart';
 import 'package:proje/cocuklarim.dart';
 import 'package:proje/esim.dart';
@@ -24,98 +25,6 @@ class AnaEkran extends StatelessWidget {
   const AnaEkran({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Benim Projem'),
-        backgroundColor: Colors.red,
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                children: [
-                  Image.asset('assets/images/ben.jpg'),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  ElevatedButton(
-                    onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const hakkimda(),
-                      ),
-                    ),
-                    child: Text("Hakkımda"),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Image.asset('assets/images/esim.jpg'),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  ElevatedButton(
-                      onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const esim(),
-                            ),
-                          ),
-                      child: Text("Eşim")),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Image.asset('assets/images/cocuklarim.jpg'),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  ElevatedButton(
-                      onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const cocuklarim(),
-                            ),
-                          ),
-                      child: Text('Çocuklarım')),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Image.asset('assets/images/lokum.jpg'),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  ElevatedButton(
-                      onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const lokum(),
-                            ),
-                          ),
-                      child: Text('Lokum')),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Image.asset('assets/images/bulut.jpg'),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  ElevatedButton(
-                      onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const bulut(),
-                            ),
-                          ),
-                      child: Text('Bulut')),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Anasayfa();
   }
 }
